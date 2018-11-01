254f1d8b6237240006bfb56fd02b28a0139379d3
aryakk04 <arya.kk@vvdntech.in>
2018-11-01 10:52:41 +0530

    Made changes based on the review comments received

--- functions/functions.py
+++ functions/functions.py
@@ -1,9 +1,15 @@
-## Function to find the type of arguments
-
 def find_type_arguments (arg1,arg2):
+	""" Prints the type of passed two arguments
+		Args:
+			arg1:  Any value 
+			arg2:  Any value
+
+		Returns:
+			No return value
+	"""
+
 	print ("Argument 1 :", type(arg1))
 	print ("Argument 2 :", type(arg2))
-	return 1
 
 input1 = input("Enter input1 : ")
 input2 = input("Enter input2 : ")
--- functions/sum.py
+++ functions/sum.py
@@ -1,13 +1,22 @@
-## Function to find the sum of arguments
-
 def sum_arguments (arg1,arg2):
+	""" Prints the sum of passed two argument
+		if both arguments have the same data type.
+
+		Args:
+			arg1:  Any value
+			arg2:  Any value
+
+		Returns:
+			sum of arguments or if both argument have
+			different data types return value is -1.
+	"""
+
 	if (type(arg1) != type(arg2)): 
 	# Since types of arguments are not same
 		return -1
 	else:
 		sumarg = arg1+arg2
-		print ("Sum of arguments is: ", sumarg)
-		return 1
+		return sumarg
 
 input1 = input("Enter input1 : ")
 input2 = input("Enter input2 : ")
@@ -16,4 +25,5 @@ return_val = sum_arguments (input1, input2)
 # Function calling to find the sum of arguments
 if (return_val == -1) :
 	print ("Both inputs should have the same data type")
-
+else:
+      print "sum of two argument", return_val
