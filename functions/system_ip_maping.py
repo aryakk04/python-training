aa837832dcd510b47d07d53816c7931a1a1fbf5c
aryakk04 <arya.kk@vvdntech.in>
2018-12-12 12:38:53 +0530

    Used python function 'min(len(list1,len(list2))' instead of if else condition

--- functions/system_ip_maping.py
+++ functions/system_ip_maping.py
@@ -5,21 +5,18 @@ def get_system_to_ip_map(system_names,system_ips):
 		then prints the list of tuple which has system name 
 		and corresponding system IP.
 		
-		Args:
-			system_names:  It is list of system names.
-			system_ips:  It is list of system IPs
+	Args:
+		system_names:  It is list of system names.
+		system_ips:  It is list of system IPs
 
-        Returns:
-            Returns list of tuple having system name and 
-			corresponding system IP.
+	Returns:
+		Returns list of tuple having system name and 
+		corresponding system IP.
     """
 
 	system_ip_map = []
 	if len(system_names) != len(system_ips) :
-		if len(system_names) < len(system_ips):
-			system_no = len(system_names)
-		else:
-			system_no = len(system_ips)
+		system_no = min(len(system_names),len(system_ips))
 	for x in range (0,system_no):
 		system_ip_map += [(system_names[x],system_ips[x])]
 	return system_ip_map
