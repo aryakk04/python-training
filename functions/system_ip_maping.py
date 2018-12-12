2bd437b0019f7f002f67155a226292b9599bd0c1
Rengaraj <sakthirengaraj@gmail.com>
2018-12-12 04:34:07 -0800

    white space is cleaned

--- functions/system_ip_maping.py
+++ functions/system_ip_maping.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python
 
 def get_system_to_ip_map(system_names,system_ips):
-	""" Get lists of system names and system IPs
-        then prints the list of tuple which has system name 
+        """ Get lists of system names and system IPs
+        then prints the list of tuple which has system name
         and corresponding system IP.
-		
-	Args:
+
+        Args:
             system_names:  It is list of system names.
-	    system_ips:  It is list of system IPs
-        
-	Returns:
-            Returns list of tuple having system name and 
-	    corresponding system IP.
+            system_ips:  It is list of system IPs
+
+        Returns:
+            Returns list of tuple having system name and
+            corresponding system IP.
         """
         system_ip_map = []
-	if not system_names or not system_ips:
+        if not system_names or not system_ips:
                 print "List is empty!!! Please enter some data"
-		return  system_ip_map
-        
-	system_no = min(len(system_names),len(system_ips))
+                return  system_ip_map
+
+        system_no = min(len(system_names),len(system_ips))
 
         for x in range (0,system_no):
-		system_ip_map += [(system_names[x],system_ips[x])]
+                system_ip_map += [(system_names[x],system_ips[x])]
 
         return system_ip_map
 
@@ -30,5 +30,3 @@ system_names = _list.split(',')
 _list = raw_input("Enter system IPs seperated by coma : ")
 system_ips = _list.split(',')
 print get_system_to_ip_map(system_names, system_ips)
-
-
