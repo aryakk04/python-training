c94ede5af45f4d0dbac73a8a043fc3e62ab93cc6
Rengaraj <sakthirengaraj@gmail.com>
2018-12-12 04:29:46 -0800

    len of sequence is changed to not of sequence i.e not system_ips or not system_names

--- functions/system_ip_maping.py
+++ functions/system_ip_maping.py
@@ -2,30 +2,33 @@
 
 def get_system_to_ip_map(system_names,system_ips):
 	""" Get lists of system names and system IPs
-		then prints the list of tuple which has system name 
-		and corresponding system IP.
+        then prints the list of tuple which has system name 
+        and corresponding system IP.
 		
 	Args:
-		system_names:  It is list of system names.
-		system_ips:  It is list of system IPs
-
+            system_names:  It is list of system names.
+	    system_ips:  It is list of system IPs
+        
 	Returns:
-		Returns list of tuple having system name and 
-		corresponding system IP.
-    """
-	system_ip_map = []
-	if (len(system_names) == 1 or len(system_ips) == 1):
-		print "List is empty!!! Please enter some data"
+            Returns list of tuple having system name and 
+	    corresponding system IP.
+        """
+        system_ip_map = []
+	if not system_names or not system_ips:
+                print "List is empty!!! Please enter some data"
 		return  system_ip_map
+        
 	system_no = min(len(system_names),len(system_ips))
-	for x in range (0,system_no):
-		system_ip_map += [(system_names[x],system_ips[x])]
-	return system_ip_map
 
-list = raw_input("Enter system names seperated by coma : ")
-system_names = list.split(',')
-list = raw_input("Enter system IPs seperated by coma : ")
-system_ips = list.split(',')
-system_ip_map = get_system_to_ip_map(system_names,system_ips)
-print system_ip_map
+        for x in range (0,system_no):
+		system_ip_map += [(system_names[x],system_ips[x])]
+
+        return system_ip_map
+
+_list = raw_input("Enter system names seperated by coma : ")
+system_names = _list.split(',')
+_list = raw_input("Enter system IPs seperated by coma : ")
+system_ips = _list.split(',')
+print get_system_to_ip_map(system_names, system_ips)
+
 
